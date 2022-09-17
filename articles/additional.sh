bed84e373b693a37871b8d8291c598ed4a66cc6b
silkrow <si1krow@outlook.com>
2022-09-17 00:11:47 -0500

    deleted articals

--- articles/additional.sh
+++
@@ -1,6 +0,0 @@
-# pandoc sample.md -t html -o sample.html
-echo "<!DOCTYPE html> <html><head> <link rel=\"stylesheet\" href=\"style.css\" /> </head><body>" > temp
-cat $1 >> temp
-cat temp>$1
-echo "</body></html>" >> $1
-rm temp
